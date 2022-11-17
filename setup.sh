930b46741f7b0a6c8fdc2e19f5e9fa4a2859e1f2
Preston Smith <psmith@avantus.com>
2022-11-16 16:40:48 -0800

    Make dotfiles append to existing files.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 THIS_DIRECTORY="$(dirname "$(realpath "$0")")"
 
 for file in "${THIS_DIRECTORY}/to_install/"*; do
-    cp "${file}" "/root/.$(basename ${file})"
+    new_file="/root/.$(basename ${file})"
+    if [ -f "${new_file}" ]; then
+        cat "${file}" >> ${new_file}
+    else
+        cp "${file}" "/root/.$(basename ${file})"
+    fi
 done
 
