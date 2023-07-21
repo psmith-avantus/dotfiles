b628a8f1d1f6bcc5246e1a197efd720b4e5de0d2
Preston Smith <psmith@avantus.com>
2023-07-21 08:58:12 -0700

    Update to work with users.

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ this_directory="$(dirname "$(realpath "$0")")"
 
 # We search for all files in the to_install/ directory of this repo.
 for file in "${this_directory}/to_install/"*; do
-    # And then determine a new location for that file (/root/.${file name})
-    new_file="/root/.$(basename ${file})"
+    # And then determine a new location for that file (${HOME}/.${file name})
+    new_file="${HOME}/.$(basename ${file})"
     
     # If the file already exists in the destination location,
     if [ -f "${new_file}" ]; then
